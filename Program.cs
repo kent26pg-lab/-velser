bb6c3615431f01d9202217bd3dc081fe52ed9d58
kent <kent.26pg@gmail.com>
2026-04-20 12:25:54 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+// get user input
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < i + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+}
+
